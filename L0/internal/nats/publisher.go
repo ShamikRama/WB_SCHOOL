3e3dcb7a82f5b5e9ed2bc949d10c39d6c2d25f75
Sonia Leena <sonialee@un-e2.kzn.21-school.ru>
2024-09-18 16:24:54 +0300

    fixed

--- L0/internal/nats/publisher.go
+++ L0/internal/nats/publisher.go
@@ -1,25 +1,31 @@
 package nats
 
 import (
-	"L0/pkg"
 	"fmt"
 	"log"
+
+	"L0/pkg"
+	"github.com/nats-io/stan.go"
 )
 
-func LoadMsgToNats(filepath string) {
-	sc, err := stan.Connect("test-cluster", "publisher-client")
+func LoadMsgToNats(filepath string, clusterID string, clientID string) error {
+	sc, err := stan.Connect(clusterID, clientID)
 	if err != nil {
-		log.Fatalln(err)
+		return fmt.Errorf("failed to connect to NATS: %w", err)
 	}
 	defer sc.Close()
+
 	msg, err := pkg.ReadJson(filepath)
 	if err != nil {
-		log.Fatalln(err)
+		return fmt.Errorf("failed to read JSON from file: %w", err)
 	}
+
 	err = sc.Publish("orders", msg)
 	if err != nil {
-		log.Fatalln(err)
-		fmt.Print("The message has not been sent")
+		log.Println("Error publishing message:", err)
+		return fmt.Errorf("failed to publish message: %w", err)
 	}
+
 	log.Println("The message has been sent")
+	return nil
 }
