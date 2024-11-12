fbeaf1b917573e520a7a6386eef9b5e848a559be
Sonia Leena <sonialee@si-g4.kzn.21-school.ru>
2024-11-13 00:31:42 +0300

    добавил задание

--- L1/L1.3/main.go
+++ L1/L1.3/main.go
@@ -1,5 +1,38 @@
-// Дана последовательность чисел: 2,4,6,8,10.
-// Найти сумму их квадратов
+// // Дана последовательность чисел: 2,4,6,8,10.
+// // Найти сумму их квадратов
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func main(){
+// 	array := []int{2,4,6,8,10}
+// 	sum := 0
+// 	channel := make(chan int)
+// 	wg := sync.WaitGroup{}
+// 	for _, val := range array{
+// 		wg.Add(1)
+// 		go func(){
+// 			defer wg.Done()
+// 			channel <- val * val
+// 		}()
+// 	}
+// 	go func(){
+// 		wg.Wait()
+// 		close(channel)
+// 	}()
+	
+// 	for val := range channel {
+// 		sum +=val
+// 	}
+// 	fmt.Println(sum)
+// }
+
+
+
 
 package main
 
@@ -13,6 +46,14 @@ func main(){
 	sum := 0
 	channel := make(chan int)
 	wg := sync.WaitGroup{}
+
+	go func(){
+		for val := range channel {
+			sum += val
+		} 
+	}()
+
+
 	for _, val := range array{
 		wg.Add(1)
 		go func(){
@@ -20,13 +61,10 @@ func main(){
 			channel <- val * val
 		}()
 	}
-	go func(){
-		wg.Wait()
-		close(channel)
-	}()
+
+	wg.Wait()
+	close(channel)
 	
-	for val := range channel {
-		sum +=val
-	}
 	fmt.Println(sum)
 }
+
