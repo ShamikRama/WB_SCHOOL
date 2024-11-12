7a9fc8ab4d49e315d682c52c4e74980d421df7af
Sonia Leena <sonialee@si-g4.kzn.21-school.ru>
2024-11-13 00:24:27 +0300

    добавил задание

---
+++ L1/L1.3/main.go
@@ -0,0 +1,32 @@
+// Дана последовательность чисел: 2,4,6,8,10.
+// Найти сумму их квадратов
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main(){
+	array := []int{2,4,6,8,10}
+	sum := 0
+	channel := make(chan int)
+	wg := sync.WaitGroup{}
+	for _, val := range array{
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			channel <- val * val
+		}()
+	}
+	go func(){
+		wg.Wait()
+		close(channel)
+	}()
+	
+	for val := range channel {
+		sum +=val
+	}
+	fmt.Println(sum)
+}
