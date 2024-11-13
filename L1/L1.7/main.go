607a3677836262ca5678c3dd84b19e5c80e0ae21
Sonia Leena <sonialee@si-i6.kzn.21-school.ru>
2024-11-13 21:29:18 +0300

    добавил задание

---
+++ L1/L1.7/main.go
@@ -0,0 +1,36 @@
+// Реализовать конкурентную запись данных в map.
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	mapa := make(map[int]int)
+	mu := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		i := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			defer mu.Unlock()
+			mapa[i] = i * i
+		}()
+	}
+
+	for key, val := range mapa {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Printf("%d - ключб, %d - значение ", key, val)
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println(mapa)
+}
