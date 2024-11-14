ee6c05f65ad276cdeb0e86e831830ec68a413c1f
Sonia Leena <sonialee@si-a5.kzn.21-school.ru>
2024-11-14 19:51:00 +0300

    добавил задание

--- L1/L1.20/main.go
+++ L1/L1.20/main.go
@@ -1,3 +1,23 @@
 // Разработать программу которая переворачивает строку
 // sun dog cat - cat dog sun
 
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := "sun     dog cat"
+	words := strings.Fields(s) // Разбиваем строку на слова
+
+	// Переворачиваем порядок слов
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	// Объединяем слова обратно в строку
+	res := strings.Join(words, " ")
+	fmt.Println(res) // Вывод: cat dog sun
+}
