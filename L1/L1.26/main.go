88e45e147539beb65dd362125876059551dee9c8
Sonia Leena <sonialee@si-a5.kzn.21-school.ru>
2024-11-15 00:27:31 +0300

    добавил задание

--- L1/L1.26/main.go
+++ L1/L1.26/main.go
@@ -1,2 +1,33 @@
 // 26. Разработать программу, которая проверяет,
-//что все символы в строке уникальные (true — если уникальные, false etc). Функция проверки должна быть регистронезависимой.
+//что все символы в строке уникальные (true — если уникальные, false etc).
+//Функция проверки должна быть регистронезависимой.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func unic(s string) bool {
+	strings.ToLower(s)
+	mapa := make(map[rune]struct{})
+	for _, elem := range s {
+		if elem != ' ' {
+			if _, ok := mapa[elem]; ok {
+				return false
+			} else {
+				mapa[elem] = struct{}{}
+			}
+		}
+	}
+	return true
+}
+
+func main() {
+	//a := "adafCVWSc"
+	//b := "ascvfgL "
+	c := "ascv  fgL  "
+	fmt.Println(unic(c))
+
+}
